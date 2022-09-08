9a49f1fc68d909b2210d6864f3850bd53e3a0d32
Baris <brsone12@gmail.com>
2022-09-08 19:22:55 +0300

    Small improvements on DriverManager and Hooks

--- src/test/java/Utils/DriverManager.java
+++ src/test/java/Utils/DriverManager.java
@@ -1,17 +1,32 @@
 package Utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class DriverManager {
     static WebDriver driver;
+
+    static void setupDriver() {
+        Logger.getLogger("").setLevel(Level.SEVERE);
+        System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "Error");
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
 
     public static WebDriver getDriver() {
         return driver;
     }
 
     public static void quitDriver() {
-        if (driver != null)
             driver.quit();
     }
 
